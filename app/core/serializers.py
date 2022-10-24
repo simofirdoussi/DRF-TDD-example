8cb7c9ede3ec35fe5b609f9307024bd311aecb8e
Mohamed <mohamed.elfirdoussi@um5r.ac.ma>
2022-10-24 15:49:11 +0100

    add Book model serializer

--- app/core/serializers.py
+++ app/core/serializers.py
@@ -1,3 +1,11 @@
 """
 Core app serializers
 """
+from rest_framework import serializers
+
+
+class BookSerializer(serializers.ModelSerializer):
+    """Book model serializer."""
+
+    class Meta:
+        fields = ['name', 'description']
