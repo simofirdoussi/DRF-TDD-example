f32052e8f7f8399b3d8f4e1c7c108150ff75a4a1
Mohamed <mohamed.elfirdoussi@um5r.ac.ma>
2022-10-24 15:47:27 +0100

    add Book model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,13 @@
 """
 Core app models.
 """
+from django.db import models
+
+
+class Books(models.Model):
+    name = models.CharField(max_length=255)
+    descritpion = models.TextField()
+
+    def __str__(self):
+        return self.name
+
